1a58185a84ee13714ca2e65fa1ce32fae22be33e
Anastasiia <root@localhost.localdomain>
2020-12-06 00:09:35 +0300

    mmm

--- lab5/mem2.bash
+++ lab5/mem2.bash
@@ -6,7 +6,7 @@ size=0
 while :; do
 array+=(1 2 3 4 5 6 7 8 9 10)
 let cnt=$cnt+1
-if [[ cnt -eq 3 ]]; then
+if [[ cnt -eq 100000 ]]; then
 echo "${#array[*]}" >> report2.log
 cnt=0
 fi
--- lab5/newmem.bash
+++ lab5/newmem.bash
@@ -1,13 +1,17 @@
 #!/bin/bash
-> report.log
 declare -a array
 cnt=0
 size=0
+if [[ $# == 0 ]]; then
+N=310000
+else 
+N=$1
+fi
+
 while :; do
 array+=(1 2 3 4 5 6 7 8 9 10)
-let cnt=$cnt+1
-if [[ cnt -eq 3 ]]; then
-echo "${#array[*]}" >> report.log
-cnt=0
+if (( "${#array[@]}" >= "$N" )); then
+echo "Success"
+exit 0
 fi
 done 
